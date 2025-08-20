9ca60b0443dca8c44db591c6dbbbe0012a2283c8
Chinmay Joshi <chinmayjoshi38@gmail.com>
2025-08-20 13:44:19 -0400

    Delete prisma directory

--- prisma/migrations/20250819213809_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Feedback" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "scenario" TEXT NOT NULL,
-    "analysis" TEXT NOT NULL,
-    "rating" INTEGER,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
